80097fecfc53c871159f00effc9f31dfda810248
marisasaunders <marisaksaunders@gmail.com>
2022-03-28 14:15:57 -0400

    finished all deliverables

--- src/components/src/TileContainer.js
+++ src/components/src/TileContainer.js
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Tile from './Tile.js'
 
 function TileContainer({hogs}) {
 
+  const [myHogs, setMyHogs] = useState(hogs)
+  const [greasedFilter, setGreasedFilter] = useState(false)
+  const [nameSort, setNameSort] = useState(false)
+  const [weightSort, setWeightSort] = useState(false)
+
+  function handleFilter(e) {
+    setGreasedFilter(!greasedFilter)
+    console.log(e.target)
+
+    if (e.target.checked) {
+      const hogsFiltered = myHogs.filter((hog) => {
+        return hog.greased === true 
+      })
+      setMyHogs(hogsFiltered)
+    } 
+    else 
+      setMyHogs(hogs)
+  }
+
+  function handleNameSort(e) {
+    setNameSort(!nameSort)
+
+    if (e.target.checked) {
+      let sortedHogsName = myHogs.slice().sort( (a,b) => {
+        if(a.name < b.name) return -1
+        if(a.name > b.name) return 1
+        return 0
+      })
+      setMyHogs(sortedHogsName)
+    }
+    else
+      setMyHogs(hogs)
+  }
+
+  function handleWeightSort(e) {
+    setWeightSort(!weightSort)
+
+    if(e.target.checked) {
+      let sortedHogsweight = myHogs.slice().sort(( a,b) => {
+        if(a.weight < b.weight) return -1
+        if(a.weight > b.weight) return 1
+        return 0
+      })
+      setMyHogs(sortedHogsweight)
+    }
+    else 
+      setMyHogs(hogs)
+  }
+
   return (
     <div>
-      <div>Only greased <input type="checkbox" id="filter"/>filter</div>
-      <div>Sort by name <input type="checkbox" id="sort"/>sort</div>
-      { hogs.map(hog => <Tile key={hog.name} hog={hog}/>) }
+      <div>Only greased: <input onClick={handleFilter} type="checkbox" id="filter"/>filter</div>
+      <div>Sort by name: <input onClick={handleNameSort} type="checkbox" id="sort"/>sort</div>
+      <div>Sort by weight: <input onClick={handleWeightSort} type="checkbox" id="sort"/>sort</div>
+      { myHogs.map(hog => <Tile key={hog.name} hog={hog}/>) }
     </div>
   )
 }
